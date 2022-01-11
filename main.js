2541bac219cb98f604fe12cc83301204413063d7
Sergio Ayala <sergio.ayala51@gmail.com>
2022-01-11 18:41:01 +0100

    improve game

--- main.js
+++ main.js
@@ -1,47 +1,157 @@
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d")
 
-ctx.beginPath();
-ctx.rect(20, 40, 53, 10);
-ctx.fillStyle = "blue";
-ctx.fill();
-ctx.closePath();
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width - paddleWidth) / 2;
 
-var x = canvas.width/2;
-var y = canvas.height-30;
+var rightPressed = false;
+var leftPressed = false;
 
-var dx = 1;
-var dy = -1;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+
+var dx = 2;
+var dy = -2;
 
 var ballRadius = 8;
 
 var collideSound;
-
 collideSound = new Audio("");
 
-function draw(){
+var score = 0
 
-    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius)
-    {
+var brickRowCount = 3;
+var brickColumnCount = 7;
+var brickWidth = 70;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 23;
+var brickOffsetLeft = 25;
+
+var bricks = [];
+for (c = 0; c < brickColumnCount; c++) {
+    bricks[c] = [];
+    for (r = 0; r < brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2, false);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawBricks() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status == 1) {
+                var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+                var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
+    drawBall()
+    drawPaddle()
+    drawScore();
+    collisionDetection();
+
+    if (y + dy < ballRadius) {
         dy = -dy;
         collideSound.play();
+    } else if (y + dy > canvas.height - ballRadius - paddleHeight) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+            collideSound.play();
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
     }
 
-    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius)
-    {
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         collideSound.play();
         dx = -dx;
     }
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2, false);
-    ctx.fillStyle = "black";
-    ctx.fill();
-    ctx.closePath();
-
     x += dx;
     y += dy;
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 5;
+    }
+    else if (leftPressed && paddleX > 0) {
+        paddleX -= 5;
+    }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth + ballRadius && y > b.y && y < b.y + brickHeight + ballRadius) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount) {
+                        alert("YOU WIN, CONGRATULATIONS!");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
 }
 
 setInterval(draw, 10)
