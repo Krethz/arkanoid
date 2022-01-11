15edaa888abc918293fb4b1b9625b5a9f26d0ee5
Krethz <andreu1986@gmail.com>
2022-01-11 17:24:29 +0100

    ball bouncing

--- main.js
+++ main.js
@@ -13,11 +13,29 @@ var y = canvas.height-30;
 var dx = 1;
 var dy = -1;
 
+var ballRadius = 8;
+
+var collideSound;
+
+collideSound = new Audio("");
 
 function draw(){
+
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius)
+    {
+        dy = -dy;
+        collideSound.play();
+    }
+
+    if(x + dx > canvas.width - ballRadius || x + dx < ballRadius)
+    {
+        collideSound.play();
+        dx = -dx;
+    }
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.beginPath();
-    ctx.arc(x, y, 5, 0, Math.PI*2, false);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2, false);
     ctx.fillStyle = "black";
     ctx.fill();
     ctx.closePath();
